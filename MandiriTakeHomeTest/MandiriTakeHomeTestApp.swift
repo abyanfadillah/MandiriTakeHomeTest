1653eee6bee561f48035f8f613cf3c99cabba2c7
abyan <abyanfadillah12@gmail.com>
2022-10-11 17:36:49 +0700

    Initial Commit

---
+++ MandiriTakeHomeTest/MandiriTakeHomeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MandiriTakeHomeTestApp.swift
+//  MandiriTakeHomeTest
+//
+//  Created by Muhammad Abyan Fadillah on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MandiriTakeHomeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
